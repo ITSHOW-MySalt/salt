9924b6f8773b3fb849eae9e0bd8c30fc4b05a237
dayeone1 <s2312@e-mirim.hs.kr>
2025-04-07 00:26:46 +0900

    JDK버전 수정 & 파일 오류 수정

---
+++ src/main/java/com/example/salt/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.salt.controller;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "test";
+    }
+}
--- src/test/java/com/example/salt/SaltApplicationTests.java
+++ src/test/java/com/example/salt/SaltApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.salt;
+package test.java.com.example.salt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
