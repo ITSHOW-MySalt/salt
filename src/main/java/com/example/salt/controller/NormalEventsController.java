d14c723fbd25fc174bd6f9e7a066c633284a778b
dayeone1 <s2312@e-mirim.hs.kr>
2025-05-11 23:34:05 +0900

    일상 이벤트 대사 불러오기

--- src/main/java/com/example/salt/controller/NormalEventsController.java
+++ src/main/java/com/example/salt/controller/NormalEventsController.java
@@ -19,9 +19,11 @@ public class NormalEventsController {
     // 처음 페이지 접근 시, 랜덤한 ID로 대사 1개 조회 후 보여줌
     @GetMapping("/dialogues/normal-events")
     public String normalEvents(Model model) {
-        int selectId = (int) (Math.random() * 18);  // 0~17 범위의 랜덤 ID
-        NormalEventsDTO dto = service.selectById(selectId);  // DTO 조회
-        model.addAttribute("dto", dto);  // 모델에 담아서 HTML로 전달
-        return "event_test";  // Thymeleaf 템플릿 페이지
+        int selectId = (int) (Math.random() * 18);  // 18은 추후 데이터 값 개수 불러오는 걸로 수정
+        NormalEventsDTO dto = service.selectById(selectId);
+        model.addAttribute("dto", dto);
+        return "/index";
     }
+
+
 }
