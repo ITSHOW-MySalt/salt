efa59c15ce2abed687b486184be13d9b1034a772
seokjieun0528 <s2307@e-mirim.hs.kr>
2025-06-07 00:42:44 +0900

    이벤트 번호 랜덤처리, 대사와 스탯 DTO 분리

---
+++ src/main/java/com/example/salt/service/EventsService.java
@@ -0,0 +1,9 @@
+package com.example.salt.service;
+
+import com.example.salt.entity.NormalEventsEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventsService {
+
+}
